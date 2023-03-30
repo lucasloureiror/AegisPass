4c4fb0c02c2eb10b8d3252b02b494550e5b7c408
Lucas <prolucasloureiro@gmail.com>
2023-03-30 00:18:00 -0300

    fix: go fmt done

--- app.go
+++ app.go
@@ -2,42 +2,41 @@ package main
 
 import (
 	"fmt"
+	"golang.design/x/clipboard"
 	"io"
 	"net/http"
 	"os"
 	"strconv"
-
-	"golang.design/x/clipboard"
 )
 
-func main(){
+func main() {
 	var password_size int
 	var err_convert error
 
-	if len(os.Args) < 2 || os.Args[1] == ""{
+	if len(os.Args) < 2 || os.Args[1] == "" {
 		fmt.Println("Enter password size")
 		fmt.Scan(&password_size)
 	} else {
-		password_size,_ = strconv.Atoi(os.Args[1])
+		password_size, _ = strconv.Atoi(os.Args[1])
 
-		if err_convert != nil{
+		if err_convert != nil {
 			panic("Not able to convert OS Arg to int")
 		}
 
 	}
-	
-	url := fmt.Sprintf("https://www.random.org/strings/?num=1&len=%d&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new", password_size) 
+
+	url := fmt.Sprintf("https://www.random.org/strings/?num=1&len=%d&digits=on&upperalpha=on&loweralpha=on&unique=on&format=plain&rnd=new", password_size)
 
 	resp, err_api := http.Get(url)
 
-	if err_api != nil{
+	if err_api != nil {
 		fmt.Println("Unable to parse random.org API")
 	}
 	password_byte, _ := io.ReadAll(resp.Body)
 
 	password := string(password_byte)
 
-	fmt.Println((string(password)))
+	fmt.Println((password))
 
-	clipboard.Write(clipboard.FmtText,[]byte(password))
-	}
+	clipboard.Write(clipboard.FmtText, []byte(password))
+}
