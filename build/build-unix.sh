e03903e266a5384069fb224f12ada8d200695e48
Lucas Loureiro <97252098+lucasloureiror@users.noreply.github.com>
2023-04-01 00:54:48 +0000

    fix: fixed name inside build script

--- build/build-unix.sh
+++ build/build-unix.sh
@@ -1,3 +1,3 @@
 go build -o ../build/bin/aegis ../cmd/aegis
 
-echo "Built sucessfuly the file named app inside bin folder"
+echo "Built sucessfuly the file named aegis inside bin folder"
--- build/build-windows.cmd
+++ build/build-windows.cmd
@@ -1,3 +1,3 @@
 go build -o ../build/bin/aegis.exe ../cmd/aegis
 
-echo "Built sucessfuly the file named app.exe inside bin folder"
+echo "Built sucessfuly the file named aegis.exe inside bin folder"
