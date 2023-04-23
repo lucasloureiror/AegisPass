878968b57f48872903551813c2fa6b8024e7a2f6
Lucas <prolucasloureiro@gmail.com>
2023-04-22 21:10:35 -0300

    fix: path of the executable is not seen as flag anymore

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -7,31 +7,32 @@ import (
 )
 
 func ParseFlags(flags *Flags) {
+	//Flags stay here just for better documentation of the options
 	flag.Bool("numeric", false, "Password with numbers only")
 	flag.Bool("credits", false, "Print random.org API credits to the user")
 	flag.Bool("help", false, "Help the user to use the CLI tool")
 	flag.Bool("standard", false, "Generate password with one upper case, one number and one special character at least.")
 
-	flag.Parse()
+	args := os.Args[1:] //Removing the path
 
-	for i := range os.Args {
+	for i := range args {
 
-		if strings.Contains(os.Args[i], "help") {
+		if strings.Contains(args[i], "help") {
 			flags.NeedHelp = true
 			return
 		}
 
-		if strings.Contains(os.Args[i], "credits") {
+		if strings.Contains(args[i], "credits") {
 			flags.PrintCredits = true
 			continue
 		}
 
-		if strings.Contains(os.Args[i], "numeric") {
+		if strings.Contains(args[i], "numeric") {
 			flags.UseOnlyNums = true
 			return
 		}
 
-		if strings.Contains(os.Args[i], "standard") {
+		if strings.Contains(args[i], "standard") {
 			flags.UseStandard = true
 			return
 		}
