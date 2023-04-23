729c7fc23b757880abaf1088816d4dcb544cdec4
Lucas <prolucasloureiro@gmail.com>
2023-04-23 00:45:21 -0300

    feat: random generator interface

---
+++ internal/generator/random.go
@@ -0,0 +1,23 @@
+package generator
+
+import (
+	"strconv"
+
+	"github.com/lucasloureiror/AegisPass/internal/config"
+	"github.com/lucasloureiror/AegisPass/internal/shuffle"
+)
+
+type random struct{}
+
+func (s *random) generate(pwd *config.Password, randomIndex []string) {
+
+	var index int
+	pwdLen := pwd.Size - len(pwd.Generated)
+
+	for i := 0; i < pwdLen; i++ {
+		index, _ = strconv.Atoi(randomIndex[i])
+		pwd.Generated = pwd.Generated + string(pwd.CharSet[index])
+	}
+	shuffle.String(&pwd.Generated)
+
+}
