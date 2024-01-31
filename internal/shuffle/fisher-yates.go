77751dfe34d9635d3d9e9004949187abc798860f
Lucas Loureiro Rodrigues <prolucasloureiro@gmail.com>
2024-01-30 21:59:14 -0300

    fix: removed println from files

--- internal/shuffle/fisher-yates.go
+++ internal/shuffle/fisher-yates.go
@@ -3,12 +3,10 @@ package shuffle
 import (
 	"crypto/rand"
 	"encoding/binary"
-	"fmt"
 )
 
 // Well, when I came back to this code the logic was already in place and didn't want to change.
 func fisherYatesSelector(arr []byte, length int) []byte {
-	fmt.Println("Lenght: ", len(arr))
 	shuffled := make([]byte, length)
 	for i := 0; i < length; i++ {
 		j := randomInt(len(arr))
@@ -19,7 +17,6 @@ func fisherYatesSelector(arr []byte, length int) []byte {
 }
 
 func randomInt(max int) int {
-	fmt.Println("Max: ", max)
 	buf := make([]byte, 4)
 	_, err := rand.Read(buf)
 	if err != nil {
