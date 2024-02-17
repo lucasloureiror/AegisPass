cc15860cd35ff46a93cb0385e9af3fa61a470a5c
Lucas Loureiro Rodrigues <prolucasloureiro@gmail.com>
2024-02-17 11:42:13 -0300

    feat: updated help message

--- internal/generator/help.go
+++ internal/generator/help.go
@@ -1,3 +1,18 @@
+/*
+Copyright 2024 github.com/lucasloureiror/AegisPass maintainers
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package generator
 
 import (
@@ -8,27 +23,28 @@ type help struct{}
 
 func (h help) generate(pwd *cli.Input) (string, int, error) {
 	helpMessage := `
-AegisPass v1.2.2
+AegisPass v2.0.0
 AegisPass source code is licensed under the Mozilla Public License 2.0 and is available at github.com/lucasloureiror/AegisPass
-Usage: aegis [password_length] [options]
+Usage: aegis [options] [password_length]
 
 Arguments:
-  password_length  The length of the password to be generated (default: 10)
+  password_length  The length of the password to be generated (default: 15)
 
 Options:
   --bulk 		   Specify the number of passwords to be generated (default: 1)
-  --offline 	   Generate random or numeric passwords without using random.org API
-  --numeric        Password with numbers only (default: password with length 10 if not specified)
+  --numeric        Generate a numeric password
   --standard       Generate password with one upper case, one number and one special character at least.
-  --credits        Print random.org API credits to the user after generating a password
-  --help           Help the user to use the CLI tool
+  --online 	   	   Generate random or numeric passwords with random.org web service
+  --credits        Print random.org web service credits to the user after generating a password
+  --help           Print the help message to the user
 
 Example:
-  aegis 12 --numeric
+  aegis --numeric 12
   aegis 8
-  aegis 10 --standard
-  aegis 10 --credits
-  aegis help
+  aegis --standard 10
+  aegis --online 15
+  aegis --online --credits 10
+  aegis --help
 `
 	return helpMessage, -1, nil
 }
